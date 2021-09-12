45d76b1909a8f54bede76f3bda2e32cca37527f1
Crowdbotics <team@crowdbotics.com>
2021-09-12 22:08:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "pine_mn_30487Identifier",
+  url: "https://pine_mn_30487Identifier.botics.co",
+  api: "https://pine_mn_30487Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
